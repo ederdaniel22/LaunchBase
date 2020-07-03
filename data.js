1693f7466e467c4b5c4c9d1e111fd195189afe98
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-07-02 22:41:01 -0400

    Criação do servidor e inclusão do Nunjucks

---
+++ data.js
@@ -0,0 +1,39 @@
+const { title } = require("process");
+
+module.exports = [{
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando CSS GRID na Prática | MAYK BRITO",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: Higher Order Functions | MAYK BRITO",
+        duration: "54 min",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O QUE É API? REST E RESTFUL? | MAYK BRITO",
+        duration: "33 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "DESVENDANDO A VARÁVEL THIS | MAYK BRITO",
+        duration: "48 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "GIT E GITHUB NA PRÁTICA | MAYK BRITO",
+        duration: "57 min",
+        price: "Free"
+    }
+]
